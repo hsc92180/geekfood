3ef8b66b612cd9d2f775be521a739e286abd6127
Himanshu Singh Chauhan <hsc92180@gmail.com>
2024-03-07 19:09:45 +0530

    add geekfood

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import HomeScreen from './Screen/HomeScreen';
+function App() {
+  return (
+    <>
+      <HomeScreen />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Screen/HomeScreen.js
@@ -0,0 +1,17 @@
+import Header from "../Components/Header/Header";
+import Hero from "../Components/Hero/Hero";
+import About from "../Components/About/About";
+import Footer from "../Components/Footer/Footer";
+
+const HomeScreen = () => {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <About />
+            <Footer />
+        </>
+    );
+};
+
+export default HomeScreen;
